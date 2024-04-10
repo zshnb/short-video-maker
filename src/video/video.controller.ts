fe0dad6044a4a9545f3972eed13bf3aee750c970
zshnb <a857681664@gmail.com>
2024-04-10 13:29:45 +0800

    add api

--- src/video/video.controller.ts
+++ src/video/video.controller.ts
@@ -1,7 +1,14 @@
 import { Controller, Post } from '@nestjs/common'
+import { VideoService } from './video.service'
 
 @Controller('video')
 export class VideoController {
+  constructor(private readonly videoService: VideoService) {}
   @Post('generate')
-  async generateShorts() {}
+  async generateShorts() {
+    await this.videoService.generateShorts()
+    return {
+      data: 'ok',
+    }
+  }
 }
